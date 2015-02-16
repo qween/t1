f1f020030a625a56b543bf2f495cacdac368d36b
pps2015 <pauline.lionel@gmail.com>
2015-02-16 12:35:45 -0500

    junk3

--- ex22_CodeCount.py
+++ ex22_CodeCount.py
@@ -1,3 +1,5 @@
+from 
+
 def code_count(code_file):
 	total_lines = 0
 	empty_lines = 0
@@ -6,4 +8,18 @@ def code_count(code_file):
 	file_object = open(code_file, 'r')
 	
 	for line in file_object
+		character_list = line.split()
 		
+		if character_list == []:
+			empty_lines += 1
+		elif character_list == '#':
+			comment_lines += 1
+		total_lines += 1
+		
+	file_object.close()
+	print "Total Lines = %d, Empty Lines = %d, Comment Lines = %d" % (
+		total_lines, empty_lines, comment_lines)
+	return total_lines, empty_lines, comment_lines
+	
+t,e,c = code_count(filename)
+
